84c6db1136dcda1a4b08182fd7838540c99c7066
Yann <yannrenard1025@gmail.com>
2024-04-15 21:35:04 +0200

    chore(prettier): indents

--- content/config.mjs
+++ content/config.mjs
@@ -1,26 +1,26 @@
 
 /**
  * @type {import("../config/siteConfig").UserConfig}
- */
+*/
 
 const config = {
-  title: "BAC Révision 2024",
-  description: "Cours et fiches pour réviser le BAC",
-  author: "Yann Renard",
-  domain: "https://bac-2024-obsidian-sync.vercel.app/",
+    title: "BAC Révision 2024",
+    description: "Cours et fiches pour réviser le BAC",
+    author: "Yann Renard",
+    domain: "https://bac-2024-obsidian-sync.vercel.app/",
 
-  navLinks: [{ href: "/Maths", name: "Mathématiques" }],
-  navLinks: [{ href: "/Philosophie", name: "Philosophie" }],
-  navLinks: [{ href: "/Physique-Chimie", name: "Physique-Chimie" }],
-  navLinks: [{ href: "/SIN", name: "SIN" }],
+    navLinks: [{ href: "/Maths", name: "Mathématiques" }],
+    navLinks: [{ href: "/Philosophie", name: "Philosophie" }],
+    navLinks: [{ href: "/Physique-Chimie", name: "Physique-Chimie" }],
+    navLinks: [{ href: "/SIN", name: "SIN" }],
 };
 
 const userConfig = {
-  navbarTitle: {
-    text: "BAC Révision 2024",
-  },
-  showComments: true,
-  showEditLink: true,
-  editLinkRoot: "https://github.com/yannouuuu/BAC-2024-Obsidian-Sync/tree/main/content",
+    navbarTitle: {
+      text: "BAC Révision 2024",
+    },
+    showComments: true,
+    showEditLink: true,
+    editLinkRoot: "https://github.com/yannouuuu/BAC-2024-Obsidian-Sync/tree/main/content",
 };
 export default {};
